19502c52b41b7285defbddaacd2e6a03b07b7b7b
Cesar Saucedo <71368732+dlablo33@users.noreply.github.com>
2023-09-04 18:07:21 -0600

    practica4
    as

--- ionic-am/src/app/app-routing.module.ts
+++ ionic-am/src/app/app-routing.module.ts
@@ -12,8 +12,20 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'presupuesto',
+    loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
+  },
+  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },
+  {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'receptor',
+    loadChildren: () => import('./receptor/receptor.module').then( m => m.ReceptorPageModule)
   },
 ];
 
--- ionic-am/src/app/presupuesto/presupuesto.page.spec.ts
+++ ionic-am/src/app/presupuesto/presupuesto.page.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { InicioPage } from './inicio.page';
+import { PresupuestoPage } from './presupuesto.page';
 
-describe('InicioPage', () => {
-  let component: InicioPage;
-  let fixture: ComponentFixture<InicioPage>;
+describe('PresupuestoPage', () => {
+  let component: PresupuestoPage;
+  let fixture: ComponentFixture<PresupuestoPage>;
 
   beforeEach(async(() => {
-    fixture = TestBed.createComponent(InicioPage);
+    fixture = TestBed.createComponent(PresupuestoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
